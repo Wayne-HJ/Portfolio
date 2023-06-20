ef31b982e4bdb8bbf171aee3f9a324fd14a38e3f
Haitao <jianght945@gmail.com>
2023-06-20 10:14:57 +1000

    add logo name for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,9 +38,10 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        {/* <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
-        </Navbar.Brand> */}
+        <Navbar.Brand href="/" className="d-flex">
+          <h3 className="purple">{" "}Wayne</h3>
+          {/* <img src={logo} className="img-fluid logo" alt="brand" /> */}
+        </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           onClick={() => {
